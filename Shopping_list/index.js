1e9f7c90a6dc4768376a9caa788bf2acab053bf2
Fantosism <businessspamfolder@gmail.com>
2018-11-01 11:33:48 -0700

    added function for shopping list

---
+++ Shopping_list/index.js
@@ -0,0 +1,41 @@
+// In terms of user experience, your shopping list app must allow users to:
+
+// enter itng the "Add item" button
+// check and uncheck items on the list by clicking the "Check" buttonems they need to purchase by entering text and hitting "Return" or clicki
+// permanently remove items from the list
+// Additionally:
+
+// You must use a CDN-hosted version of jQuery
+// Put your application code in a file called index.js and link to it in index.html
+// Be sure to put both script elements at the bottom of the <body> element.
+// Do not alter index.html or main.css other than adding the links to the external JavaScript inside index.html. Write JavaScript code that works with the existing HTML and CSS to 
+// implement the required features.
+// Hint: you may find it helpful to read up on and use the following jQuery methods: .submit(), preventDefault(), toggleClass(), and closest().
+
+$(function() {
+
+$('#js-shopping-list-form').submit(function(event) {
+
+  event.preventDefault();
+
+  $('.js-shopping-list-entry').val();
+
+  $('.shopping-list').append(
+    `<li>
+    <span class="shopping-item">${$('.js-shopping-list-entry').val()}</span>
+    <div class="shopping-item-controls">
+      <button class="shopping-item-toggle">
+        <span class="button-label">check</span>
+      </button>
+      <button class="shopping-item-delete">
+        <span class="button-label">delete</span>
+      </button>
+    </div>
+    </li>`
+  )}
+  console.log(('.js-shopping-list-entry').val());
+}
+)
+
+
+// Hello are we synched?
