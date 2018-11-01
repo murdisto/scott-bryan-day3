c379f5b40acd77cc5ca4489db837ec34ee95f1b3
Fantosism <businessspamfolder@gmail.com>
2018-11-01 14:47:56 -0700

    Cat Carousel completed

--- CatCarousel/index.js
+++ CatCarousel/index.js
@@ -1,1 +1,14 @@
 /*eslint-env jquery*/
+'use strict';
+
+function thumbNails() {
+  // console.log('Expecting Thumbnails');
+  $('.thumbnail').on('click', function() {
+    const imgSrc = $(this).find('img').attr('src');
+    const altImg = $(this).find('img').attr('alt');
+    // console.log('Function executed');
+    $('.hero img').attr('src', imgSrc).attr('alt', altImg);
+    // console.log($(this).find('img').attr('src').attr('alt'));
+  });
+}
+$(thumbNails);
